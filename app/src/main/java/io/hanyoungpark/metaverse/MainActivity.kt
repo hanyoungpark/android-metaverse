a08ff18b94a3df5a50cc421a0f442a635a1ff1f5
Hanyoung Park <hanyoungpark@gmail.com>
2021-09-28 23:17:38 -0700

    MET-3 Add an android service for the messanging (#2)

--- app/src/main/java/io/hanyoungpark/metaverse/MainActivity.kt
+++ app/src/main/java/io/hanyoungpark/metaverse/MainActivity.kt
@@ -2,6 +2,9 @@ package io.hanyoungpark.metaverse
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
