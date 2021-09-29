01ced5fba6e8b916f90a3e4044e7426f4da1f8c5
Hanyoung Park <hanyoungpark@gmail.com>
2021-09-29 01:39:40 -0700

    MET-6 Add the login activity
    There's only design
    Applied the dark and the light theme

--- app/src/main/java/io/hanyoungpark/metaverse/MainActivity.kt
+++ app/src/main/java/io/hanyoungpark/metaverse/MainActivity.kt
@@ -1,14 +1,21 @@
 package io.hanyoungpark.metaverse
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import com.google.firebase.messaging.FirebaseMessaging
+import io.hanyoungpark.metaverse.view.membership.LoginActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val loginIntent = Intent(this, LoginActivity::class.java).apply {
+            flags  = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
+        }
+        startActivity(loginIntent)
     }
 }
---
+++ app/src/main/java/io/hanyoungpark/metaverse/view/membership/LoginActivity.kt
@@ -0,0 +1,12 @@
+package io.hanyoungpark.metaverse.view.membership
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import io.hanyoungpark.metaverse.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
