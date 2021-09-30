ecb2553145646492d6909bea50457219b0099473
Hanyoung Park <hanyoungpark@gmail.com>
2021-09-30 01:09:08 -0700

    MET-8 Add the bottom sheet dialog as the sign up screen

--- app/src/main/java/io/hanyoungpark/metaverse/view/membership/LoginActivity.kt
+++ app/src/main/java/io/hanyoungpark/metaverse/view/membership/LoginActivity.kt
@@ -2,11 +2,18 @@ package io.hanyoungpark.metaverse.view.membership
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import io.hanyoungpark.metaverse.R
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
+
+    fun signUpButtonClicked(sender: View) {
+        val signUpDialog = SignUpDialog()
+        signUpDialog.show(supportFragmentManager, null)
     }
 }
---
+++ app/src/main/java/io/hanyoungpark/metaverse/view/membership/SignUpDialog.kt
@@ -0,0 +1,19 @@
+package io.hanyoungpark.metaverse.view.membership
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import io.hanyoungpark.metaverse.R
+
+class SignUpDialog : BottomSheetDialogFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val v = inflater.inflate(R.layout.dialog_signup, container, false)
+        return v
+    }
+}
